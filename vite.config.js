b4a0bef9b4c95907a3c553fe4295ee09d646595a
Alan N <53200557+alann13@users.noreply.github.com>
2023-09-03 06:44:02 -0400

    config mod

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-const outDir = resolve(__dirname, 'dist')
-
 export default defineConfig({
   build: {
-    outDir,
-    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
